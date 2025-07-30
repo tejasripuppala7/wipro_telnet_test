640f623f75c85caf194cc1769c954092dc0d07d2
tejasripuppala7 <tejasripuppala7@gmail.com>
2025-07-30 17:42:03 +0530

    Create swaping.java

---
+++ swaping.java
@@ -0,0 +1,13 @@
+public class SwapWithTemp {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
