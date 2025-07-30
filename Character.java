1a0fe1276644c3bf861e08489ced33b23ca9889d
tejasripuppala7 <tejasripuppala7@gmail.com>
2025-07-30 17:53:13 +0530

    Character.java

---
+++ Character.java
@@ -0,0 +1,11 @@
+public class CharExample {
+    public static void main(String[] args) {
+        char letter = 'A';
+        char digit = '7';
+        char symbol = '$';
+
+        System.out.println("Letter: " + letter);
+        System.out.println("Digit: " + digit);
+        System.out.println("Symbol: " + symbol);
+    }
+}
