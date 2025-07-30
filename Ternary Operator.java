130489cb91c2d77477e5e70718496e873f57c391
tejasripuppala7 <tejasripuppala7@gmail.com>
2025-07-30 17:57:27 +0530

    Create Ternary Operator.java

---
+++ Ternary Operator.java
@@ -0,0 +1,9 @@
+public class TernaryExample {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum is: " + max);
+    }
+}
