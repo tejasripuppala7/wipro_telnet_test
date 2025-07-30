1e094890924380a02cdce291af11dbd05bc26cc9
tejasripuppala7 <tejasripuppala7@gmail.com>
2025-07-30 18:05:20 +0530

    Create arithmeticDemo.java

---
+++ arithmeticDemo.java
@@ -0,0 +1,12 @@
+public class ArithmeticDemo {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Addition (a + b): " + (a + b));
+        System.out.println("Subtraction (a - b): " + (a - b));
+        System.out.println("Multiplication (a * b): " + (a * b));
+        System.out.println("Division (a / b): " + (a / b));       // integer division
+        System.out.println("Modulus (a % b): " + (a % b));         // remainder
+    }
+}
