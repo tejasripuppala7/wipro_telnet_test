213f7fe12f374becbe209b1c7534990f92e0c9a3
tejasripuppala7 <tejasripuppala7@gmail.com>
2025-07-30 17:55:21 +0530

    Create fibonacciseries.java

---
+++ fibonacciseries.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 10; // number of terms to print
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
